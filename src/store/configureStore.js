08d85ce00525799d85f978684be0e753441985c7
Rudi van Litsenborgh <rvlglobe@gmail.com>
2018-09-11 20:41:32 +0100

    setup redux extension for chrome

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState){
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableStateInvariant()),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))        
     );
 }
